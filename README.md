613fc174c84ca1b2d6f4d13cbcb2adeab9ee705b
Harshita Nahta <60339899+Harshita-nahta@users.noreply.github.com>
2024-10-20 14:35:52 +0530

    Harshita Nahta Portfolio Website

---
+++ README.md
@@ -0,0 +1,4 @@
+# Harshita Nahta Portfolio
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Harshita-Nahta-the-lessful/pen/QWeMNVQ](https://codepen.io/Harshita-Nahta-the-lessful/pen/QWeMNVQ).
+
---
+++ dist/script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("mobile-menu-toggle")
+  .addEventListener("click", function () {
+    const navbarList = document.getElementById("navbar-list");
+    navbarList.classList.toggle("show");
+  });
